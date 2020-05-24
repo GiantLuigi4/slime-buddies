42a9c499a3897b4f96a20fbd785a7c1a1b6b357e
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-05-24 16:36:17 -0400

    Finished slime growth serum.

--- src/main/java/com/ult1team/slimebuddies/SlimeBuddies/Items/SlimeGrowthSerum.java
+++ src/main/java/com/ult1team/slimebuddies/SlimeBuddies/Items/SlimeGrowthSerum.java
@@ -1,20 +1,29 @@
 package com.ult1team.slimebuddies.SlimeBuddies.Items;
 
+import com.ult1team.slimebuddies.SlimeBuddies.SlimeBuddies;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
+import org.apache.logging.log4j.Level;
 
 public class SlimeGrowthSerum extends ModeledItemBase {
     @Override
     public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
         int currentsize = target.serializeNBT().getInteger("Size");
-    if (target instanceof EntitySlime) {
-        if (currentsize < 4) target.serializeNBT().setInteger("Size",currentsize+1);
-        return true;
-    }
+        if (target instanceof EntitySlime) {
+            if (!playerIn.world.isRemote) {
+                if (currentsize < 4) {
+                    NBTTagCompound compound=target.serializeNBT();
+                    compound.setInteger("Size",currentsize+1);
+                    target.deserializeNBT(compound);
+                }
+            }
+            return true;
+        }
         return super.itemInteractionForEntity(stack, playerIn, target, hand);
     }
 }
